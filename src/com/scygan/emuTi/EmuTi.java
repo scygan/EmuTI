62ff1bb4f32093a6f2c6606ebaa3ad6fbc264d33
Slawomir Cygan <slawomir.cygan@gmail.com>
2012-06-22 00:43:10 +0200

    retab

--- src/com/scygan/emuTi/EmuTi.java
+++ src/com/scygan/emuTi/EmuTi.java
@@ -37,12 +37,12 @@ import android.view.Window;
 
 public class EmuTi extends Activity
 {
-	EmuTiView emutiView;
+    EmuTiView emutiView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-    	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         emutiView = new EmuTiView(this);
         setContentView(emutiView);
@@ -50,14 +50,14 @@ public class EmuTi extends Activity
     
     @Override
     public void onPause() {
-    	emutiView.stop();
-    	super.onPause();
+        emutiView.stop();
+        super.onPause();
     }
     
     @Override
     public void onResume() {
-    	emutiView.start();
-    	super.onResume();
+        emutiView.start();
+        super.onResume();
     }
 
     /* load our native library */
@@ -71,190 +71,190 @@ class EmuTiView extends SurfaceView implements SurfaceHolder.Callback {
     private EmuTiNative mNative;
     
     public EmuTiView(Context context) {
-    	super(context);
+        super(context);
         getHolder().addCallback(this);
         mNative = new EmuTiNative(getContext().getAssets(), getHolder(), this);
         setFocusable(true);
     }
 
-	public void stop() {
-		 boolean retry = true;
-		 mNative.requestStop();
-		 while (retry) {
-	    	 try {
-	    		 mNativeThread.join();
-	             retry = false;
-	         } catch (InterruptedException e) {
-	                // we will try it again and again...
-	         }
-	     }
-	}
-	
-	public void start() {
-		if (mNativeThread == null || !mNativeThread.isAlive()) {
-			mNativeThread = new Thread(mNative);
-			mNativeThread.start();
-		}
-	}
-	
-	@Override protected void onDraw(Canvas canvas) {
-    	mNative.onDraw(canvas);
-    	super.onDraw(canvas);
+    public void stop() {
+         boolean retry = true;
+         mNative.requestStop();
+         while (retry) {
+             try {
+                 mNativeThread.join();
+                 retry = false;
+             } catch (InterruptedException e) {
+                    // we will try it again and again...
+             }
+         }
+    }
+    
+    public void start() {
+        if (mNativeThread == null || !mNativeThread.isAlive()) {
+            mNativeThread = new Thread(mNative);
+            mNativeThread.start();
+        }
+    }
+    
+    @Override protected void onDraw(Canvas canvas) {
+        mNative.onDraw(canvas);
+        super.onDraw(canvas);
     }
     
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-    	if (mNativeThread != null  && mNativeThread.isAlive()) {
-    		return mNative.onTouch(event);
-    	}
-    	return false;    	
+        if (mNativeThread != null  && mNativeThread.isAlive()) {
+            return mNative.onTouch(event);
+        }
+        return false;       
     }
 
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		mNative.onSurfaceResize(width, height);
-	}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mNative.onSurfaceResize(width, height);
+    }
 
-	public void surfaceCreated(SurfaceHolder arg0) {
-		start();
-	}
+    public void surfaceCreated(SurfaceHolder arg0) {
+        start();
+    }
 
-	public void surfaceDestroyed(SurfaceHolder arg0) {
-		stop();
-	}
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        stop();
+    }
 
 }
 
 class TouchEvent {
-	public TouchEvent(int x, int y, boolean pressed){
-		this.x = x;
-		this.y = y;
-		this.pressed = pressed;
-	}
-	int x, y;
-	boolean pressed;
+    public TouchEvent(int x, int y, boolean pressed){
+        this.x = x;
+        this.y = y;
+        this.pressed = pressed;
+    }
+    int x, y;
+    boolean pressed;
 }
 
 class EmuTiNative implements Runnable {
     final int W = 480;
     final int H = 800;
-	private SurfaceHolder mSurfaceHolder;
-	private Bitmap mBitmap;
-	private AssetManager mAssetManager;
-	EmuTiView mView;
-	private Semaphore mTouchLock = new Semaphore(1);
-	private Queue<TouchEvent> mTouchEventQueue = new LinkedList<TouchEvent>();
-	float mXScale, mYScale, mXOffset;
-	boolean mStopExpected = false;
-	
+    private SurfaceHolder mSurfaceHolder;
+    private Bitmap mBitmap;
+    private AssetManager mAssetManager;
+    EmuTiView mView;
+    private Semaphore mTouchLock = new Semaphore(1);
+    private Queue<TouchEvent> mTouchEventQueue = new LinkedList<TouchEvent>();
+    float mXScale, mYScale, mXOffset;
+    boolean mStopExpected = false;
+    
     /* Implemented by libemuti.so */
-	private static native void nativeEntry(Bitmap  bitmap, AssetManager assetManager, EmuTiNative emuTiNative);
-	
+    private static native void nativeEntry(Bitmap  bitmap, AssetManager assetManager, EmuTiNative emuTiNative);
+    
     /* Implemented by libemuti.so */
-	private static native void nativeStop();
-	
-	public EmuTiNative(AssetManager assetManager, SurfaceHolder surfaceHolder, EmuTiView view) {
-		mBitmap = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
-		mBitmap.setDensity(DisplayMetrics.DENSITY_DEFAULT);
-		mAssetManager = assetManager;
-		mSurfaceHolder = surfaceHolder;
-		mView = view;
+    private static native void nativeStop();
+    
+    public EmuTiNative(AssetManager assetManager, SurfaceHolder surfaceHolder, EmuTiView view) {
+        mBitmap = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
+        mBitmap.setDensity(DisplayMetrics.DENSITY_DEFAULT);
+        mAssetManager = assetManager;
+        mSurfaceHolder = surfaceHolder;
+        mView = view;
         onSurfaceResize(W, H);
-	}
-	
-	public boolean onTouch(MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_DOWN) {
-			try {
-				mTouchLock.acquire();
-				mTouchEventQueue.add(new TouchEvent((int)(event.getX() / mXScale - mXOffset),
-						(int)(event.getY() / mYScale), event.getAction() == MotionEvent.ACTION_DOWN));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			mTouchLock.release();
-			return true;
-		}
-		return false;
-	}
-	
-	public void onSurfaceResize(int width, int height) {
+    }
+    
+    public boolean onTouch(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_DOWN) {
+            try {
+                mTouchLock.acquire();
+                mTouchEventQueue.add(new TouchEvent((int)(event.getX() / mXScale - mXOffset),
+                        (int)(event.getY() / mYScale), event.getAction() == MotionEvent.ACTION_DOWN));
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            mTouchLock.release();
+            return true;
+        }
+        return false;
+    }
+    
+    public void onSurfaceResize(int width, int height) {
         mYScale = ((float)height)/mBitmap.getHeight();
         if (width > height) {
-			mXScale = ((float)height)/mBitmap.getHeight();
-			mXOffset = (((float)width)/mXScale - ((float)mBitmap.getWidth()))/2.0f;
-		} else {
-			mXScale = ((float)width)/mBitmap.getWidth();
+            mXScale = ((float)height)/mBitmap.getHeight();
+            mXOffset = (((float)width)/mXScale - ((float)mBitmap.getWidth()))/2.0f;
+        } else {
+            mXScale = ((float)width)/mBitmap.getWidth();
             mXOffset = 0.0f;
-		}
-	}
-	
-	public void onDraw(Canvas c) {
-    	c.scale(mXScale, mYScale);
-		c.drawBitmap(mBitmap, mXOffset, 0, null);
-	}
-	
-	public void requestStop() {
-		mStopExpected = true;
-		nativeStop();
-	}
-	
-	public void run() {
-		mStopExpected = false;
-		nativeEntry(mBitmap, mAssetManager, this);
-		if (!mStopExpected) {
-			//calculator was powered off by user
-			mView.post(new Runnable() {
+        }
+    }
+    
+    public void onDraw(Canvas c) {
+        c.scale(mXScale, mYScale);
+        c.drawBitmap(mBitmap, mXOffset, 0, null);
+    }
+    
+    public void requestStop() {
+        mStopExpected = true;
+        nativeStop();
+    }
+    
+    public void run() {
+        mStopExpected = false;
+        nativeEntry(mBitmap, mAssetManager, this);
+        if (!mStopExpected) {
+            //calculator was powered off by user
+            mView.post(new Runnable() {
                 public void run() {
-                	Context ctx = mView.getContext();
-                	Resources res = ctx.getResources();
-                	AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
-                	alertDialog.setTitle(res.getString(R.string.calc_powered_off));
-                	alertDialog.setButton(res.getString(R.string.power_on), new DialogInterface.OnClickListener() {
-                	      public void onClick(DialogInterface dialog, int which) {
-                 	      //here you can add functions
-                	      mView.stop();
-                	      mView.start();
-                	} }); 
-                	//alertDialog.setIcon(R.drawable.icon);
-                	alertDialog.show();
+                    Context ctx = mView.getContext();
+                    Resources res = ctx.getResources();
+                    AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+                    alertDialog.setTitle(res.getString(R.string.calc_powered_off));
+                    alertDialog.setButton(res.getString(R.string.power_on), new DialogInterface.OnClickListener() {
+                          public void onClick(DialogInterface dialog, int which) {
+                          //here you can add functions
+                          mView.stop();
+                          mView.start();
+                    } }); 
+                    //alertDialog.setIcon(R.drawable.icon);
+                    alertDialog.show();
                 }
             });
-		}
-	}
-	
-	/* Called by libemuti.so */
-	private TouchEvent getTouch() { //called from native thread
-		TouchEvent ret = null;
-		try {
-			mTouchLock.acquire();
-			if (!mTouchEventQueue.isEmpty()) {
-				ret = mTouchEventQueue.remove();
-			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		mTouchLock.release();
-		return ret; 
-	}
-	
-	/* Called by libemuti.so */
-	private void flip() { //called from native thread
-		Canvas c = null;
+        }
+    }
+    
+    /* Called by libemuti.so */
+    private TouchEvent getTouch() { //called from native thread
+        TouchEvent ret = null;
+        try {
+            mTouchLock.acquire();
+            if (!mTouchEventQueue.isEmpty()) {
+                ret = mTouchEventQueue.remove();
+            }
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }               
+        mTouchLock.release();
+        return ret; 
+    }
+    
+    /* Called by libemuti.so */
+    private void flip() { //called from native thread
+        Canvas c = null;
         try {
             c = mSurfaceHolder.lockCanvas(null);
             synchronized(mSurfaceHolder) {
-            	onDraw(c);
+                onDraw(c);
             }
         } finally {
             // do this in a finally so that if an exception is thrown
             // during the above, we don't leave the Surface in an
             // inconsistent state
             if (c != null) {
-            	mSurfaceHolder.unlockCanvasAndPost(c);
+                mSurfaceHolder.unlockCanvasAndPost(c);
             }
-	    }
-	}
+        }
+    }
 }
 
 
